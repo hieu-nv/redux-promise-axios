d5f23910aa68d482e3314dd5c848c17029296b61
HieuNV <hieunv@live.com>
2016-12-04 08:57:33 +0700

    v0.0.4

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ import Axios from "axios";
 const [ GET, PUT, POST, DELETE ]  = ["GET", "PUT", "POST", "DELETE"];
 
 export default ({dispatch, getState}) => next => action => {
-  if (!action.url) {
+  if (typeof action.payload === "object" && !action.payload.url) {
     return next(action);
   }
   switch (action.type.substring(0, action.type.indexOf("_"))) {
     case GET:
+      const url = typeof action.payload === "string" ? action.payload : action.payload.url;
+      const params = typeof action.payload !== "string" && action.payload.params ? action.payload.params : {};
       return dispatch({
         type: action.type,
-        payload: Axios.get(action.url, action.payload ? action.payload : {})
+        payload: Axios.get(url, params)
       });
     case PUT:
       return dispatch({
