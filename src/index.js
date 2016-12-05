84d651d1689fb29ab9041c758d2813da2e2a7ad8
HieuNV <hieunv@live.com>
2016-12-05 11:37:36 +0700

    UT

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ export default ({dispatch, getState}) => next => action => {
   if (!action.payload || typeof action.payload === "object" && !action.payload.url) {
     return next(action);
   }
+
+  const url = typeof action.payload === "string" ? action.payload : action.payload.url;
+  const params = typeof action.payload !== "string" && action.payload.params ? action.payload.params : {};
+
   switch (action.type.substring(0, action.type.indexOf("_"))) {
     case GET:
-      const url = typeof action.payload === "string" ? action.payload : action.payload.url;
-      const params = typeof action.payload !== "string" && action.payload.params ? action.payload.params : {};
       return dispatch({
         type: action.type,
         payload: Axios.get(url, params)
@@ -17,17 +19,17 @@ export default ({dispatch, getState}) => next => action => {
     case PUT:
       return dispatch({
         type: action.type,
-        payload: Axios.put(action.url, action.payload)
+        payload: Axios.put(url, params)
       });
     case POST:
       return dispatch({
         type: action.type,
-        payload: Axios.post(action.url, action.payload)
+        payload: Axios.post(url, params)
       });
     case DELETE:
       return dispatch({
         type: action.type,
-        payload: Axios.delete(action.url, action.payload)
+        payload: Axios.delete(url, params)
       });
     default:
       return next(action);
